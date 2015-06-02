fcaad3ceafb7f8de4996eaa4c89864e26fbb78bc
Radhwane Chebaane <radcheb@gmail.com>
2015-06-02 09:29:09 +0200

    docker + latex

---
+++ latex.sh
@@ -0,0 +1,11 @@
+#!bin/sh
+
+
+sudo apt-get install texlive
+sudo apt-get install texlive-full
+
+# gedit plugin
+sudo apt-get install gedit-latex-plugin
+
+#Recommended LaTeX Packages
+sudo apt-get install gedit-latex-plugin texlive-fonts-recommended latex-beamer texpower texlive-pictures texlive-latex-extra texpower-examples imagemagick
