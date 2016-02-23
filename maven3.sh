f6f5def04d1c8e88adc3fcf86631d382265ff5fe
radcheb <radcheb@gmail.com>
2016-02-23 13:13:57 +0100

    force installation

--- maven3.sh
+++ maven3.sh
@@ -3,6 +3,6 @@
 sudo echo "deb http://ppa.launchpad.net/natecarlson/maven3/ubuntu precise main" >> /etc/apt/sources.list 
 sudo echo "deb-src http://ppa.launchpad.net/natecarlson/maven3/ubuntu precise main" >> /etc/apt/sources.list 
 
-sudo apt-get update && sudo apt-get install maven3
+sudo apt-get update && sudo apt-get -y install maven3
 
 sudo rm -f /usr/bin/mvn && sudo ln -s /usr/share/maven3/bin/mvn /usr/bin/mvn
